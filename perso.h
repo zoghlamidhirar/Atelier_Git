fd551f6eee2ff8cc1d90d49d69dc4aea50297860
sarraaaaaaa <sarra.benaissa@esprit.tn>
2022-03-03 06:57:12 -0800

    perso.h

---
+++ perso.h
@@ -0,0 +1,27 @@
+#include <SDL/SDL.h>
+#include<SDL/SDL_image.h>
+#define SCREEN_H
+#define SCREEN_W
+
+struct image
+{
+SDL_Rect pos1;
+SDL_Rect pos2;
+SDL_Surface *imag
+}image;
+
+typedef image image;
+
+deftype struct
+{
+image i[30];
+SDL _*Surface
+SDL _Rect 
+int score
+int direction
+}perso;
+
+void init (perso p1);
+void afficherperso(perso p1,SDL_Surface
+	
+ 
